d2944aa820ee1ffde0feac954cd9d4ad00d0b127
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-04-01 00:10:35 -0400

    init: added initial project + solution

---
+++ Mobile_App/LockIt/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LockIt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
