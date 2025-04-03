db9fb98caf755abebafd4089c58f165af763bced
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-04-03 19:45:37 -0400

    chore: fixed fonts setup

--- Mobile_App/LockIt/MauiProgram.cs
+++ Mobile_App/LockIt/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace LockIt
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Jersey15-Regular.ttf", "Jersey15Regular");
                 });
 
 #if DEBUG
