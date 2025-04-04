94412544256cd1c734f1a6c5d57c99adb06582a4
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-04-03 20:45:59 -0400

    feat: added user model

--- Mobile_App/LockIt/Models/User.cs
+++ Mobile_App/LockIt/Models/User.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace LockIt.Models
 {
-    internal class User
+    public class User
     {
+        public string Uid { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
     }
 }
+
