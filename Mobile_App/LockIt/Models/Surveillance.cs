f2f99501c8a36ed5fdec22d71b5356c3262b9892
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-04-04 19:59:29 -0400

    docs: finished model xml documentation

--- Mobile_App/LockIt/Models/Surveillance.cs
+++ Mobile_App/LockIt/Models/Surveillance.cs
@@ -6,50 +6,73 @@ using System.Text;
 using System.Threading.Tasks;
 using AForge.Video;
 using AForge.Video.DirectShow;
+
 namespace LockIt.Models
 {
+    /// <summary>
+    /// Provides functionality for handling motion detection, camera feed access, and GPS data retrieval.
+    /// </summary>
     internal class Surveillance
     {
-
         private int _motionSensorPin;
-        //TODO: Figure out how the data from the camera will be used.
+
+        // TODO: Figure out how the data from the camera will be used.
         private int _camera;
+
         private GpioController _motionSensor = new GpioController();
         private int _GPS;
 
-        //TODO: Add validation
-        public Surveillance(int motionSensorPin) 
-        { 
-           _motionSensorPin = motionSensorPin;  
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Surveillance"/> class with the specified GPIO pin for motion detection.
+        /// </summary>
+        /// <param name="motionSensorPin">The GPIO pin connected to the motion sensor.</param>
+        // TODO: Add validation
+        public Surveillance(int motionSensorPin)
+        {
+            _motionSensorPin = motionSensorPin;
         }
-        public int Camera { get { return _camera; } 
-            set {
-               
-                _camera = value;
-            } }
+
+        /// <summary>
+        /// Gets or sets the identifier for the camera device.
+        /// </summary>
+        public int Camera
+        {
+            get { return _camera; }
+            set { _camera = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the GPIO controller used for the motion sensor.
+        /// </summary>
         public GpioController MotionSensor
         {
             get { return _motionSensor; }
-            set
-            {
-                _motionSensor = value;
-
-            }
+            set { _motionSensor = value; }
         }
+
+        /// <summary>
+        /// Gets or sets the GPS data reference.
+        /// </summary>
         public int GPS
         {
             get { return _GPS; }
-            set
-            {
-                _GPS = value;
-
-            }
+            set { _GPS = value; }
         }
 
+        /// <summary>
+        /// Detects whether motion has been triggered via the motion sensor.
+        /// </summary>
+        /// <returns><c>true</c> if motion is detected; otherwise, <c>false</c>.</returns>
+        /// <example>
+        /// <code>
+        /// var surveillance = new Surveillance(17);
+        /// bool motionDetected = surveillance.IsMotion();
+        /// </code>
+        /// </example>
         public bool IsMotion()
         {
             _motionSensor.OpenPin(_motionSensorPin, PinMode.Input);
-            if ( _motionSensor.Read(_motionSensorPin) == PinValue.High)
+            if (_motionSensor.Read(_motionSensorPin) == PinValue.High)
             {
                 Console.WriteLine("Motion detected");
                 return true;
@@ -61,13 +84,26 @@ namespace LockIt.Models
             }
         }
 
-        //TODO: Figure out how to get GPS related data from the rapsberry Pi.
+        // TODO: Figure out how to get GPS related data from the Raspberry Pi.
         public void GetGPSData()
         {
 
         }
 
-  
+        /// <summary>
+        /// Retrieves the first available video capture device (e.g., camera).
+        /// </summary>
+        /// <returns>A <see cref="VideoCaptureDevice"/> object representing the camera, or <c>null</c> if none found.</returns>
+        /// <example>
+        /// <code>
+        /// var surveillance = new Surveillance(17);
+        /// var camera = surveillance.GetCamera();
+        /// if (camera != null)
+        /// {
+        ///     camera.Start();
+        /// }
+        /// </code>
+        /// </example>
         public VideoCaptureDevice GetCamera()
         {
             var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
@@ -80,7 +116,6 @@ namespace LockIt.Models
             {
                 return new VideoCaptureDevice(videoDevices[0].MonikerString);
             }
-            
         }
     }
 }
