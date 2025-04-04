b9e122eb533ca2b022bfbb193a0f6d0bad77b9e4
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-04-03 21:58:32 -0400

    feat: added firebase user model

---
+++ Mobile_App/LockIt/Models/FirebaseAuthResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockIt.Models
+{
+    public class FirebaseAuthResponse
+    {
+        public string idToken { get; set; }
+        public string email { get; set; }
+        public string refreshToken { get; set; }
+        public string expiresIn { get; set; }
+        public string localId { get; set; }
+    }
+}
+
