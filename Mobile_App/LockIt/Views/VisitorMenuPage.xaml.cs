391c24364a6b9fca5dbd642c28be8fbe9c4ed323
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-04-04 20:22:57 -0400

    docs: updated .xaml.cs file documentation

--- Mobile_App/LockIt/Views/VisitorMenuPage.xaml.cs
+++ Mobile_App/LockIt/Views/VisitorMenuPage.xaml.cs
@@ -1,19 +1,48 @@
-namespace LockIt.Views;
-
-public partial class VisitorMenuPage : ContentPage
+namespace LockIt.Views
 {
-	public VisitorMenuPage()
-	{
-		InitializeComponent();
-	}
-
-    private async void OnAccessCameraClicked(object sender, EventArgs e)
+    /// <summary>
+    /// Represents the menu interface for a visitor user, providing options such as accessing the camera or opening a lock.
+    /// </summary>
+    public partial class VisitorMenuPage : ContentPage
     {
-        await Shell.Current.GoToAsync("CameraPage");
-    }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VisitorMenuPage"/> class and loads its components.
+        /// </summary>
+        public VisitorMenuPage()
+        {
+            InitializeComponent();
+        }
 
-    private async void OnOpenLockClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("OpenLockPage"); 
+        /// <summary>
+        /// Handles the event when the "Access Camera" button is clicked.
+        /// Navigates the user to the <c>CameraPage</c>.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Event arguments.</param>
+        /// <example>
+        /// <code>
+        /// OnAccessCameraClicked(this, EventArgs.Empty);
+        /// </code>
+        /// </example>
+        private async void OnAccessCameraClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("CameraPage");
+        }
+
+        /// <summary>
+        /// Handles the event when the "Open Lock" button is clicked.
+        /// Navigates the user to the <c>OpenLockPage</c>.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Event arguments.</param>
+        /// <example>
+        /// <code>
+        /// OnOpenLockClicked(this, EventArgs.Empty);
+        /// </code>
+        /// </example>
+        private async void OnOpenLockClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("OpenLockPage");
+        }
     }
 }
