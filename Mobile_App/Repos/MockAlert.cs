8a7aef8bc35ed56f0fa45083ba5caf4e1b379a64
Liam Galley <119807591+LiamGalley@users.noreply.github.com>
2025-04-07 14:12:58 -0400

    Project running.

--- Mobile_App/Repos/MockAlert.cs
+++ Mobile_App/Repos/MockAlert.cs
@@ -12,19 +12,19 @@ namespace ComfuritySolutions.Repos
         public Task<List<Alert>> GetSecurityAlertsAsync()
         {
             return Task.FromResult(new List<Alert>
-        {
-            new Alert { AlertMessage = "Door 1 unlocked @ 08:30 AM" },
-            new Alert { AlertMessage = "Motion detected in hallway" }
-        });
+            {
+                new Alert("Door 1 unlocked @ 08:30 AM"),
+                new Alert ("Motion detected in hallway")
+            });
         }
 
         public Task<List<Alert>> GetComfortAlertsAsync()
         {
             return Task.FromResult(new List<Alert>
-        {
-            new Alert { AlertMessage = "Temperature at 26°C" },
-            new Alert { AlertMessage = "Humidity reached 60%" }
-        });
+            {
+                new Alert ("Temperature at 26°C"),
+                new Alert ("Humidity reached 60%")
+            });
         }
     }
 }
