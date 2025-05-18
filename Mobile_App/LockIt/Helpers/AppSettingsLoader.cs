01394fc2132e29b63c29566417b8ae0b4c3c5db8
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-05-18 17:27:19 -0400

    fix: moved appsettings.json to embedded for mobile use

---
+++ Mobile_App/LockIt/Helpers/AppSettingsLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LockIt.Helpers
+{
+    public static class AppSettingsLoader
+    {
+        public static JsonElement Load()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames()
+                                       .FirstOrDefault(name => name.EndsWith("appsettings.json"));
+
+            if (resourceName is null)
+                throw new FileNotFoundException("Embedded appsettings.json not found in assembly resources.");
+
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var doc = JsonDocument.Parse(stream);
+            return doc.RootElement.Clone(); 
+        }
+    }
+
+}
