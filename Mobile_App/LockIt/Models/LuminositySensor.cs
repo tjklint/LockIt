c41068bdb0c9e5ca6ddf48e3052aea0becad5846
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-05-19 01:32:55 -0400

    docs: documented rest of models

--- Mobile_App/LockIt/Models/LuminositySensor.cs
+++ Mobile_App/LockIt/Models/LuminositySensor.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Team Name: LockIt
+// Team Members: Dylan Savelson, Joshua Kravitz, Timothy (TJ) Klint
+// Description: Represents luminosity sensor data including RGB channels, infrared, and proximity values.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +10,9 @@ using System.Threading.Tasks;
 
 namespace LockIt.Models
 {
+    /// <summary>
+    /// Represents readings from a luminosity sensor, including RGB, infrared, and proximity data.
+    /// </summary>
     public class LuminositySensor
     {
         private double _infraRed;
@@ -14,10 +21,49 @@ namespace LockIt.Models
         private double _red;
         private double _proximity;
 
-        public double infraRed { get { return _infraRed; } set { _infraRed = value; } }
-        public double Green { get { return _green; } set { _green = value; } }
-        public double Blue { get { return _blue; } set { _blue = value; } }
-        public double Red { get { return _red; } set { _red = value; } }    
-        public double Proximity { get { return _proximity; } set { _proximity = value; } } 
+        /// <summary>
+        /// Gets or sets the infrared light intensity value.
+        /// </summary>
+        public double infraRed
+        {
+            get { return _infraRed; }
+            set { _infraRed = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the green light intensity value.
+        /// </summary>
+        public double Green
+        {
+            get { return _green; }
+            set { _green = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the blue light intensity value.
+        /// </summary>
+        public double Blue
+        {
+            get { return _blue; }
+            set { _blue = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the red light intensity value.
+        /// </summary>
+        public double Red
+        {
+            get { return _red; }
+            set { _red = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the proximity value indicating how close an object is to the sensor.
+        /// </summary>
+        public double Proximity
+        {
+            get { return _proximity; }
+            set { _proximity = value; }
+        }
     }
 }
